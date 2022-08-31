093ea425000b1313a832d7130fbc36219cbcb283
ugne <ugnetomkute@gmail.com>
2022-08-31 17:17:20 +0300

    color / input

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -8,10 +8,19 @@ function App() {
 
     const [read, setRead] = useState(null);
 
+    //KONTROLIUOJAMA INPUTAS
+    const [text1, setText1] = useState('');
+
+    const [color, setColor] = useState('#57ac30');
+
+    const [backgr, setBackgr] = useState ('#57ac30');
+
     const save = () => {
         localStorage.setItem('number', count);
         const jobj = JSON.stringify ({cat: 'big'});
         localStorage.setItem('obj', jobj)
+        
+
     }
 
     const readNumber = () => {
@@ -33,17 +42,36 @@ function App() {
     }, []);
 //kai tik uzsikrauna puslapis nukaito sia reiksme: setRead(localStorage.getItem('number')
 
+
+    useEffect (() => {
+        localStorage.setItem('text', text1);
+
+    }, [text1]);
+
+
+        const changeText1 = (e) => {
+            // console.log('bandau keisti', e.target.value)
+            setText1(e.target.value);
+        }
+//e.target rodo kas ta eventa iskviete
+
+
     return (
         <div className="App">
-          <header className="App-header">
+          <header className="App-header" style={{backgroundColor: backgr}}>
             <>
            
-            <h1>Pirmadienis Nr: {count}</h1>
-            <h2>Nuskaityta Nr: {read}</h2>
+            <h1>Pirmadienis Nr: {count} Nuskaityta Nr: {read}</h1>
+            <div className="dog-bin">
             <button onClick={() => setCount (c => c + 1)}>+1</button>
             <button onClick={save}>save</button>
             <button onClick={readNumber}>read</button>
-    
+            <button onClick={() => setBackgr(color)}>DO BACKGR</button>
+
+            </div>
+
+            <input type="text" value={text1} onChange={changeText1}></input>
+            <input type="color" value={color} onChange={e => setColor(e.target.value)}></input>
     
          </>
           </header>
