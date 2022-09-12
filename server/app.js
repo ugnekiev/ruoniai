222425fe3ce742d9d9a1f47b241a704b19086b38
ugne <ugnetomkute@gmail.com>
2022-09-12 15:52:13 +0300

    ruo / sql / cors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3003;
+//naudojam cors (npm i cors)
+const cors = require("cors");
+app.use(cors());
+//naudojam sql (npm i mysql)
+const mysql = require("mysql");
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
+
+//conection su duomenu baze
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "ruo_nei_ka",
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Labas, Bebrai!')
+})
+
+app.get('/ate', (req, res) => {
+  res.send('Ate, Bebrai!')
 })
 
 app.listen(port, () => {
