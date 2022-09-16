1ec5d8410c2a0b0dc001774532901bc287259e7c
ugne <ugnetomkute@gmail.com>
2022-09-16 14:17:18 +0300

    update mini crud

--- movies/src/Components/Line.jsx
+++ movies/src/Components/Line.jsx
@@ -4,26 +4,19 @@ import DataContext from "./DataContext";
 
 function Line({movie}) {
 
-    const{setDeleteData} = useContext(DataContext);
+    const{setDeleteData, setModalData} = useContext(DataContext);
 
     return (
         <li className="list-group-item">
             
             <div className="movie">
                 <div className="movie__content">
-                    <div className="movie__content__title">
-                        {movie.title}
-                    </div>
-                    <div className="movie__content__genre">
-                        {genres.find(g => g.id === movie.genre)?.type}
-                    </div>
-                    <div className="movie__content__year">
-                        {movie.year}
-                    </div>
-
+                    <div className="movie__content__title">{movie.title}</div>
+                    <div className="movie__content__genre">{genres.find(g => g.id === movie.genre)?.type}</div>
+                    <div className="movie__content__year">{movie.year}</div>
                 </div>
                 <div className="movie__buttons">
-                <button type="button" class="btn btn-outline-primary">Edit</button>
+                <button onClick={() => setModalData(movie)}type="button" class="btn btn-outline-primary">Edit</button>
                 <button onClick={() => setDeleteData(movie)} type="button" className="btn btn-outline-danger">Delete</button>
                 </div>
             </div>
